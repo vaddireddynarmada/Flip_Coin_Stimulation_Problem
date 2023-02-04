54d3d80915b061bde2e7331e5f608f0b4dd7a31e
unknown <narmada.vaddireddy97@gmail.com>
2023-02-04 20:04:12 +0530

    flip coin 21 times

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-number=1;
-while [ $number -le 11 ]
+headcount=0;
+tailcount=0;
+head=0;
+tail=0;
+count=0;
+while [ $head -lt 21 ] && [ $tail -lt 21 ]
 do
-if [ number -eq 0 ]
- echo "head";
-((number++));
+ flipcoin=$(($RANDOM%2))
+while [ $flipcoin -eq 0 ]
+do 
+ echo "Head "
+head=$(($headcount+1))
 break;
-else
- echo "tail";
-((number++));
-break;
-fi
 done
+echo "Tails"
+tail=$(($tailcount+1))
+break
+while [ $head -eq 21 ] -o [ $tail -eq 21 ]
+do
+break
+done
+if [ $head -gt $tail ]
+then
+ echo "head win"
+break;
+ellif[ $tail -gt $head]
+
+echo "tail win"
+break;
+else "tie"
+fi
+count++;
+done
+echo "head won number of time " $head;
+echo "tail won number of times " $tail;
 
